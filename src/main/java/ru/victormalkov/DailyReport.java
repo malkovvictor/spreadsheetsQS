98da2cdd64d8110dee9bbd0306eb70bd6046e117
Victor Malkov <malkov.victor@gmail.com>
2021-10-10 12:56:35 +0300

    add DailyReport class to save array of days

---
+++ src/main/java/ru/victormalkov/DailyReport.java
@@ -0,0 +1,19 @@
+package ru.victormalkov;
+
+import java.util.ArrayList;
+
+public class DailyReport {
+    private ArrayList<Day> days;
+
+    public Day getDay(int number) {
+        if (number <= days.size()) {
+            return days.get(number - 1);
+        } else {
+            return null;
+        }
+    }
+
+    public void pushDay(Day day) {
+        days.add(day);
+    }
+}
