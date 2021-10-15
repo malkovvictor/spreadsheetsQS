5f719b7582abfa075f8ad32635e8e548b6971e4f
Victor Malkov <malkov.victor@gmail.com>
2021-10-15 19:38:41 +0300

    add new spreadsheet id

--- src/main/java/ru/victormalkov/reportchecker/ReportChecker.java
+++ src/main/java/ru/victormalkov/reportchecker/ReportChecker.java
@@ -9,8 +9,10 @@ import java.security.GeneralSecurityException;
 
 public class ReportChecker {
     public static void main(String[] args) throws GeneralSecurityException, IOException {
-        final String spreadsheetId = "1OPVrXiPLh3LtM8rIveEPhUJQjgEtyM08T7i5bvYE3Gw";
+        //final String spreadsheetId = "1OPVrXiPLh3LtM8rIveEPhUJQjgEtyM08T7i5bvYE3Gw";
         //final String spreadsheetId = "1-VoVp6VvzeAQZgmMv4Bx0yGq9Q0GrBk-sK6AUtgE7NM";
+
+        final String spreadsheetId = "1qjqjJt3Adb9SaoiNtPxjvichhUJd4G2tPnzpurGNgGg";
 
         DailySellReportReader sellReportReader = new DailySellReportReader();
         DailyReport report = sellReportReader.readReport(spreadsheetId);
@@ -18,6 +20,7 @@ public class ReportChecker {
             System.out.print(d.toPrettyString());
         }
 
+        System.in.read();
     }
 
 }
