3c72498cd279cb28accc7db8e52778d4b48ed24a
Victor Malkov <malkov.victor@gmail.com>
2021-11-07 21:05:16 +0300

    load spreadsheet in another thread

--- src/main/java/ru/victormalkov/reportchecker/ui/ResultFormController.java
+++ src/main/java/ru/victormalkov/reportchecker/ui/ResultFormController.java
@@ -1,6 +1,8 @@
 package ru.victormalkov.reportchecker.ui;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import ru.victormalkov.reportchecker.service.DailyReport;
 import ru.victormalkov.reportchecker.service.DailySellReportReader;
@@ -13,19 +15,32 @@ public class ResultFormController {
     @FXML
     private TextArea myTextArea;
 
+    @FXML
+    private Label labelIsCounting;
+
     public void initialize() throws IOException, GeneralSecurityException {
-        String spreadsheetId = ReportCheckerUI.spreadsheetId;
+        final String spreadsheetId = ReportCheckerUI.spreadsheetId;
         //final String spreadsheetId = "1OPVrXiPLh3LtM8rIveEPhUJQjgEtyM08T7i5bvYE3Gw";
         //final String spreadsheetId = "1-VoVp6VvzeAQZgmMv4Bx0yGq9Q0GrBk-sK6AUtgE7NM";
-
         //final String spreadsheetId = "1qjqjJt3Adb9SaoiNtPxjvichhUJd4G2tPnzpurGNgGg";
 
-        DailySellReportReader sellReportReader = new DailySellReportReader();
-        DailyReport report = sellReportReader.readReport(spreadsheetId);
-        for (Day d : report.getDays()) {
-            myTextArea.appendText(d.toPrettyString());
-        }
+        Task task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                DailySellReportReader sellReportReader = new DailySellReportReader();
+                DailyReport report = sellReportReader.readReport(spreadsheetId);
+                for (Day d : report.getDays()) {
+                    myTextArea.appendText(d.toPrettyString());
+                }
+                return null;
+            }
 
-        myTextArea.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            @Override
+            protected void succeeded() {
+                super.succeeded();
+                labelIsCounting.setVisible(false);
+            }
+        };
+        new Thread(task).start();
     }
 }
