0354b096c45572fdcb05357a93f04af5379ece32
Victor Malkov <malkov.victor@gmail.com>
2022-01-15 13:59:52 +0300

    use progress indicator instead of label fix #9

--- src/main/java/ru/victormalkov/reportchecker/ui/ResultFormController.java
+++ src/main/java/ru/victormalkov/reportchecker/ui/ResultFormController.java
@@ -3,29 +3,26 @@ package ru.victormalkov.reportchecker.ui;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextArea;
 import ru.victormalkov.reportchecker.service.DailyReport;
 import ru.victormalkov.reportchecker.service.DailySellReportReader;
 import ru.victormalkov.reportchecker.service.Day;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 
 public class ResultFormController {
     @FXML
     TextArea myTextArea;
 
     @FXML
-    Label labelIsCounting;
+    ProgressIndicator progressIndicator;
 
-    public void initialize() throws IOException, GeneralSecurityException {
+    public void initialize() {
         final String spreadsheetId = ReportCheckerUI.spreadsheetId;
         //final String spreadsheetId = "1OPVrXiPLh3LtM8rIveEPhUJQjgEtyM08T7i5bvYE3Gw";
         //final String spreadsheetId = "1-VoVp6VvzeAQZgmMv4Bx0yGq9Q0GrBk-sK6AUtgE7NM";
         //final String spreadsheetId = "1qjqjJt3Adb9SaoiNtPxjvichhUJd4G2tPnzpurGNgGg";
 
-        Task task = new Task<Void>() {
+        Task<Void> task = new Task<>() {
             @Override
             protected Void call() throws Exception {
                 DailySellReportReader sellReportReader = new DailySellReportReader();
@@ -41,7 +38,7 @@ public class ResultFormController {
             @Override
             protected void succeeded() {
                 super.succeeded();
-                Platform.runLater(() -> labelIsCounting.setVisible(false));
+                Platform.runLater(() -> progressIndicator.setVisible(false));
             }
         };
         new Thread(task).start();
